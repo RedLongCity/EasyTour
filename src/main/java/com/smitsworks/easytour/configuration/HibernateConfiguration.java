1fa5a4c3f0cd7eb9709a2c0576b2d6479fbc9d62
Vladimir_Kharchenko <RedLongCity@gmail.com>
2017-12-05 22:27:19 +0200

    Changed MySQL url for saving and getting data in UTF-8

--- src/main/java/com/smitsworks/easytour/configuration/HibernateConfiguration.java
+++ src/main/java/com/smitsworks/easytour/configuration/HibernateConfiguration.java
@@ -51,9 +51,9 @@ public class HibernateConfiguration {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-        properties.put("hibernate.connection.useUnicode", environment.getRequiredProperty("hibernate.connection.useUnicode"));
-        properties.put("hibernate.connection.characterEncoding", environment.getRequiredProperty("hibernate.connection.characterEncoding"));
-        properties.put("hibernate.connection.charSet", environment.getRequiredProperty("hibernate.connection.charSet"));
+//        properties.put("hibernate.connection.useUnicode", environment.getRequiredProperty("hibernate.connection.useUnicode"));
+//        properties.put("hibernate.connection.characterEncoding", environment.getRequiredProperty("hibernate.connection.characterEncoding"));
+//        properties.put("hibernate.connection.charSet", environment.getRequiredProperty("hibernate.connection.charSet"));
         return properties;
     }
 
